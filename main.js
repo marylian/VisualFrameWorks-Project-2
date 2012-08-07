33034c06a59ea01ba82b2c6251d2812328d45d8f
Marylia Nieves <marylian@fullsail.edu>
2012-08-06 23:54:58 -0400

    Added a function to populate the values in the drop down menu

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-//Marylia Nieves
+   //Marylia Nieves
 //Visual Frameworks (VFW) 
 //Assignment 2
 // 8/09/12 
+var chameleon = "blue";
+alert (chameleon);
 
-//Wait until the DOM s ready
+//Wait until the DOM is ready
 window.addEventListener ("DOMContentLoaded", function(){
 	
 
@@ -12,15 +14,32 @@ window.addEventListener ("DOMContentLoaded", function(){
 	 	var theElement = document.getElementById (x);
 	 	return theElement;
 	 }
-	
+	//Create select field elements and populate with options.
+	function makeCats (){
+		 var formTag=document.getElementsByTagName ("form"),
+		 selectLi=$('categories'),
+		 makeSelect=document.createElement ('select');
+		 makeSelect.attributes("id", "category");
+		 for (i=0; i<taskCategories.length; i++){
+		 	var makeOption=document.createElement('option');
+			var optText=taskCategories[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML=optText;
+			makeSelect.appendChild(makeOption);
+		 }
+		 selectLi.appendChild(makeSelect);
+	}
 	//Variable defaults
-	
+	var taskCategories=["Choose a task", "Grade papers", "Contact parents", "Meetings", "Write lesson plans", "Test", "Projects", "Others"]
+	makeCats();
 	
 	//Set Link and Submit Click Events 
-	var displayTask= $('displayTask');
-	displayTask.addEventListener ("click", getData);
+/*	var displayTask= $('displayTask');
+	displayTask.addEventListener ("click", getData); 
 	var clearLink= $('clearTask');
 	clearLink.addEventListener ("click",clearLocal);
-	var save=$ ('submit');
-	save.addEventListener ("click",storeData);
+	var save=$ ('taskSubmit');
+	save.addEventListener ("click",storeData);*/
 });
+
+
