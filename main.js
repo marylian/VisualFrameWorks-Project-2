76dcbeb399651ccdf21a3d2626339ee35b201947
Marylia Nieves <marylian@fullsail.edu>
2012-08-07 20:41:18 -0400

    Gathered all of the form fields and stored in an object

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ window.addEventListener ("DOMContentLoaded", function(){
 		 var formTag=document.getElementsByTagName ("form"),
 		 selectLi=$('categories'),
 		 makeSelect=document.createElement ('select');
-		 makeSelect.setAttribute("id", "category");
+		  makeSelect.setAttribute("id", "category");
 		 for (i=0; i<taskCategories.length; i++){
 		 	var makeOption=document.createElement('option');
 			var optText=taskCategories[i];
@@ -30,17 +30,37 @@ window.addEventListener ("DOMContentLoaded", function(){
 		 }
 		 selectLi.appendChild(makeSelect);
 	}
+	
+	function storeData (){
+		//localStorage.setItem("test","hello");
+		var id 					=Math.floor(Math.random()*100000001);
+		//Gather up all our form field values and store in an object.
+		//Object properties contain array with the form label and input value.
+		var item				= {};
+			
+			//item.checkbox
+			item.sub			= ["Subject:", $('sub').value];
+			item.period			= ["Period:", $('period').value];
+			item.grade			= ["Grade Level:", $('grade').value];
+			item.category		= ["Categories:", $('category').value];
+			item.date			= ["Due Date:", $('dueDate').value];
+			item.comments		= ["My Notes:", $('comments').value];
+		//Save data into Local Storage:
+		
+			
+	}
+	 
 	//Variable defaults
 	var taskCategories=["Choose a task", "Grade papers", "Contact parents", "Meetings", "Write lesson plans", "Test", "Projects", "Others"];
 	makeCats();
 	
 	//Set Link and Submit Click Events 
-/*	var displayTask= $('displayTask');
-	displayTask.addEventListener ("click", getData); 
-	var clearLink= $('clearTask');
-	clearLink.addEventListener ("click",clearLocal);
+	//var displayTask= $('displayTask');
+	//displayTask.addEventListener ("click", getData); 
+	//var clearLink= $('clearTask');
+	//clearLink.addEventListener ("click",clearLocal);
 	var save=$ ('taskSubmit');
-	save.addEventListener ("click",storeData);*/
+	save.addEventListener ("click",storeData);
 });
 
 
