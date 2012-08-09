6f468e7cb121ea349e6d1d06cb0f9af8fb31d6b5
Marylia Nieves <marylian@fullsail.edu>
2012-08-08 22:31:44 -0400

    clear data, display data, submit task updated

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-   //Marylia Nieves
+//Marylia Nieves
 //Visual Frameworks (VFW) 
 //Assignment 2
 // 8/09/12 
-var chameleon = "blue";
-alert (chameleon);
+//var chameleon = "blue";
+//alert (chameleon);
 
 //Wait until the DOM is ready
 window.addEventListener ("DOMContentLoaded", function(){
@@ -15,7 +15,7 @@ window.addEventListener ("DOMContentLoaded", function(){
 	 	return theElement;
 	 }
 	//Create select field elements and populate with options.
-	
+
 	function makeCats (){
 		 var formTag=document.getElementsByTagName ("form"),
 		 selectLi=$('categories'),
@@ -30,7 +30,7 @@ window.addEventListener ("DOMContentLoaded", function(){
 		 }
 		 selectLi.appendChild(makeSelect);
 	}
-	
+
 	//Find the value of selected checkboxes
 	function getSelectedCheckbox () {
 		var checkbox =document.forms [0].weekday;
@@ -40,7 +40,28 @@ window.addEventListener ("DOMContentLoaded", function(){
 			}
 		}
 	}
-	
+
+	function toggleControls (m) {
+		switch (m) {
+			case "on":
+				$('taskForm').style.display="none";
+				$('clearTask').style.display="inline";
+				$ ('displayTask').style.display="none;"
+				$ ('addTask').style.display="inline";
+				break;
+			case "off":
+				$('taskForm').style.display="block";
+				$('clearTask').style.display="inline";
+				$('displayTask').style.display="inline";
+				$('addTask').style.display="none";
+				$('cats').style.display="none";
+				break;
+			default:
+				return false;
+			
+			
+		}
+	} 
 	function storeData (){
 		//localStorage.setItem("test","hello");
 		var id 					=Math.floor(Math.random()*100000001);
@@ -58,22 +79,67 @@ window.addEventListener ("DOMContentLoaded", function(){
 		//Save data into Local Storage: Use Stringify to convert our object to a string.
 		localStorage.setItem  (id, JSON.stringify (item));
 		alert ("Task Saved!");
-			
+
 	}
-	 
+
+
+function getData (){
+	toggleControls ("on");
+	if(localStorage===0){
+		alert("There is no task to display");
+	}
+		//write data from local storage to the browser.
+		var makeDiv = document.createElement ('div');
+		makeDiv.setAttribute ("id", "cats");
+		var makeList = document.createElement ('ul');
+		makeDiv.appendChild (makeList);
+		document.body.appendChild(makeDiv);
+        $('cats').style.display="block";
+		for (var i=0, len=localStorage.length; i<len; i++) { 
+			var makeli = document.createElement ('li');
+			makeList.appendChild (makeli);
+			var key = localStorage.key (i);
+			var value = localStorage.getItem (key);
+			//convert string from local storage  value back to an object by using JSON.parse
+			var obj = JSON.parse (value);
+			var makeSubList = document.createElement ('ul');
+			makeli.appendChild (makeSubList);
+			for (var m in obj){
+				var makeSubli = document.createElement ('li');
+				makeSubList.appendChild (makeSubli);
+				var optSubText = obj [m] [0]+ " " +obj [m] [1];
+				makeSubli.innerHTML =optSubText;
+			}
+		}
+	}
+
+
+	function clearLocal() {
+		if (localStorage.length===0){
+			alert("There is no task to clear.");
+		}
+		else{
+			localStorage.clear ();
+			alert("All contacts are deleted!");
+			window.location.reload();
+			return false;
+		}
+		 
+		
+	}
+	
 	//Variable defaults
 	var taskCategories=["Choose a task", "Grade papers", "Contact parents", "Meetings", "Write lesson plans", "Test", "Projects", "Others"],
 		dayValue;
-	
+
 	makeCats();
 	
 	//Set Link and Submit Click Events 
-	//var displayTask= $('displayTask'); 
-	//displayTask.addEventListener ("click", getData); 
-	//var clearLink= $('clearTask');
-	//clearLink.addEventListener ("click",clearLocal);
-	var save=$ ('taskSubmit');
+	var displayTask= $('displayTask'); 
+	displayTask.addEventListener ("click", getData); 
+	var clearLink= $('clearTask');
+	clearLink.addEventListener ("click", clearLocal);
+	var save= $('taskSubmit');
 	save.addEventListener ("click",storeData);
 });
-
 
